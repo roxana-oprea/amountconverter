46074e6c66c288b23f33da9455f7b4812c080252
Roxana Oprea <oprearoxanaadolfina@gmail.com>
2019-11-29 15:01:34 +0200

    use of render prop pattern

--- src/Amount.js
+++ src/Amount.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-function Amount(props) {
-  return (
-    <div>
-      <h3>Us Dollar: {props.amount}</h3>
-      <button onClick={props.increment}>Increment</button>
-      <button onClick={props.decrement}>Decrement</button>
-    </div>
-  );
+class Amount extends React.Component {
+  state = {
+    amount: 0,
+  };
+
+  increment = () => {
+    this.setState(prevState => ({ amount: prevState.amount + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(prevState => ({ amount: prevState.amount - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h3>Us Dollar: {this.state.amount}</h3>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        {this.props.children(this.state.amount)}
+      </div>
+    );
+  }
 }
 
 export default Amount;
--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import Amount from './Amount';
 import Euro from './Euro';
 import Pounds from './Pounds';
 
-class App extends React.Component {
-  state = {
-    amount: 0,
-  };
-
-  increment = () => {
-    this.setState(prevState => ({ amount: prevState.amount + 1 }));
-  };
-
-  decrement = () => {
-    this.setState(prevState => ({ amount: prevState.amount - 1 }));
-  };
-
-  render() {
-    return (
-      <div>
-        <Amount amount={this.state.amount} increment={this.increment} decrement={this.decrement} />
-        <Euro amount={this.state.amount} />
-        <Pounds amount={this.state.amount} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Amount>
+        {amount => (
+          <div>
+            <Euro amount={amount} />
+            <Pounds amount={amount} />
+          </div>
+        )}
+      </Amount>
+    </div>
+  );
 }
 
 export default App;
